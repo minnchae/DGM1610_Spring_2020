494e24aa9678abd9cbe89b9c20c86608db9b96ad
Ashe Rubio <rubioashlei@gmail.com>
2020-01-15 17:17:23 -0700

    edited my variable script

---
+++ unity 1 project/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    //Declaration
+
+    public float amount;
+    public float money;
+    public float cost;
+
+
+    //Declaration and Initialization
+    int cookieBox = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cookies(amount,money,cost);
+        
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void Cookies(int amount, float money, float cost)   
+    {
+        float total;
+
+        total = money - cost * amount
+        print(total);
+    }
+}
