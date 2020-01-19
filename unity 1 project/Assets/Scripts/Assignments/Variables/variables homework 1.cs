07c6aa7ee56de7434b1b1b8e2ec019e4fb58abad
Ashe Rubio <rubioashlei@gmail.com>
2020-01-18 23:15:56 -0700

    adding 1 of 5 homework scripts

---
+++ unity 1 project/Assets/Scripts/Assignments/Variables/variables homework 1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variableshomework1 : MonoBehaviour
+{
+    float waterBottle;
+    float waterAmount;
+    float waterCost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterAmount * waterCost = waterBottle;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
