caeccd73f2a5cf08950cf8938263ea119d81a427
Ashe Rubio <rubioashlei@gmail.com>
2020-02-07 00:54:15 -0700

    adding a pickup

---
+++ unity 1 project/Assets/Scripts/Assignments/Variables/Pickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public int pointsToAdd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        // Check to see if trigger detected player
+        if(other.gameObject.CompareTag("Player")){
+
+            ScoreManager.AddPoints (pointsToAdd);
+            Destroy (gameObject);
+        }
+    }
+}
