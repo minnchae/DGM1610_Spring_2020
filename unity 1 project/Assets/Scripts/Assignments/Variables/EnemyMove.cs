eda36248acd297f70a9c0431d516ebfeb3bfdafa
Ashe Rubio <rubioashlei@gmail.com>
2020-02-24 17:10:45 -0700

    adding more to enemy move script

--- unity 1 project/Assets/Scripts/Assignments/Variables/EnemyMove.cs
+++ unity 1 project/Assets/Scripts/Assignments/Variables/EnemyMove.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    public Transform target;
+    public GameObject player;
+    private Rigidbody enemyRb;
     public float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+
+        //target = GameObject.Find("Player").transform; // Transform.Translate example
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        //transform.LookAt(target);
+        //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         
         
     }
+
+    void FixedUpdate(){
+        //change velocity
+            //enemyRb.velocity = player * moveSpeed;
+        //add force
+            enemyRb.AddForce((player.transform.position - transform.position).normalized * moveSpeed);
+
+        //Move position
+            //enemyRb.MovePosition((player.transform.position - transform.position).normalized * moveSpeed);
+
+    }
+    // homework for feb 24/25 play around with velocity, force and move position in 
+    //order to figure out how to move objects in different ways. 
 }
+
+
