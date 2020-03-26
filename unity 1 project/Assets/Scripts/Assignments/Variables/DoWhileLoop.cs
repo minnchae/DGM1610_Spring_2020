ea6137870bbdffe20b5d0e06629a7935c100f337
Ashe Rubio <rubioashlei@gmail.com>
2020-03-25 21:32:08 -0600

    adding loops

---
+++ unity 1 project/Assets/Scripts/Assignments/Variables/DoWhileLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+
+            print("Hello World");
+        
+
+        }
+        while (shouldContinue == true);
+        
+    }
+}
