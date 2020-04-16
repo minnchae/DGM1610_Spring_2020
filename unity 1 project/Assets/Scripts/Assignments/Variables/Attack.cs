e35ff9636cc216dfbeb1a25b2834701b30bb9cd6
Ashe Rubio <rubioashlei@gmail.com>
2020-04-15 23:08:48 -0600

    adding player health

---
+++ unity 1 project/Assets/Scripts/Assignments/Variables/Attack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int damage;
+    public Transform target;
+    
+    public float moveSpeed;
+
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.name == "Player"){
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+
+        }
+    }
+
+    void OnCollisionEnter(Collider other){
+        print("Enemy us attacking!");
+        var health = other.gameObject.GetComponent<PlayerHealth>();
+
+        if(health != null){
+            health.TakeDamage(damage);
+        }
+    }
+  
+}
